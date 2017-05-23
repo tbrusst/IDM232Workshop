ce9074130649cf9dc2d8041bb21800e82043ca12
BrendanRusso <BrendanRusso@n3-106-126.dhcp.drexel.edu>
2017-05-23 19:23:34 -0400

    1

--- index.php
+++ index.php
@@ -9,29 +9,49 @@
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="normalize.css">
 	<link href="https://fonts.googleapis.com/css?family=Muli|Playball" rel="stylesheet">
-	
+
 
 
 </head>
 <body>
 
 	<div class="wrapper">
-		
+
 		<div class="hero">
-			<img src="images/wine_image-07.jpg" alt="wine cellar">			
+			<img src="images/wine_image-07.jpg" alt="wine cellar">
 		</div>
-		
+
 		<div class="content">
-			
+
 				<h1>Wines</h1>
 
 				<div class="wines">
-					<img src="" alt="wine">
+					<?php
+
+						while ($row = mysqli_fetch_array($result)) {
+
+
+						$build = '<img src="'.$row['img'].'">';
+						$build .= "<h1>" . "$row['name']" . "</h1>";
+						$build .= "<h2>" . "$row['score']" . "</h2>";
+						$build .= "<h2>" . "$row['price']" . "</h2>";
+						$build .= "<h2>" . "$row['year']" . "</h2>";
+						$build .= "<p>" . "$row['description']" . "</p>";
+
+
+						echo $build;
+					};
+					?>
+
+
+
+
+
 				</div>
 
 		</div>
 
 	</div>
-	
+
 </body>
 </html>
