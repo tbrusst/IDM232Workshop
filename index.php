21f48a725c2df62f8e54a3842fdec0a46e7d7829
vls48 <vls48@drexel.edu>
2017-05-23 18:52:56 -0400

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Fine Wine</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/5.0.0/normalize.css">
+	<link href="https://fonts.googleapis.com/css?family=Muli|Playball" rel="stylesheet">
+	
+
+
+</head>
+<body>
+	
+</body>
+</html>
