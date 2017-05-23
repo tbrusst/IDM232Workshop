7689591894ca1b19d8a524250697e984148d7973
vls48 <vls48@drexel.edu>
2017-05-23 19:07:52 -0400

    index
    hero, header and one wine entry, ready for php inserted info

--- index.php
+++ index.php
@@ -2,15 +2,36 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+
+	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
 	<title>My Fine Wine</title>
 	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/5.0.0/normalize.css">
+	<link rel="stylesheet" href="normalize.css">
 	<link href="https://fonts.googleapis.com/css?family=Muli|Playball" rel="stylesheet">
 	
 
 
 </head>
 <body>
+
+	<div class="wrapper">
+		
+		<div class="hero">
+			<img src="images/wine_image-07.jpg" alt="wine cellar">			
+		</div>
+		
+		<div class="content">
+			
+				<h1>Wines</h1>
+
+				<div class="wines">
+					<img src="" alt="wine">
+				</div>
+
+		</div>
+
+	</div>
 	
 </body>
 </html>
